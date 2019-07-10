b0dc1c9ee0eeb68c40ad57f39911d4e2de0107d5
Michael Hecher <mh@zbox.upc.at>
2019-07-10 16:28:05 +0200

    Cleanup performance improvements

--- include/disk.hpp
+++ include/disk.hpp
@@ -51,6 +51,7 @@ namespace {
         return y - y0 < artifact_size * 0.5 ? y0 : y0 + artifact_size;
     }
 
+
     template <typename real_t>
     inline real_t clip(const real_t value, const real_t min, const real_t max)
     {
@@ -106,81 +107,6 @@ namespace {
         const auto circle_area = filter_radius * filter_radius * pi;
         return clip(area / circle_area, 0.0, 1.0);
     }
-
-
-    // template <typename return_t, typename real_t>
-    // inline return_t filter(const real_t center_x,
-    //                        const real_t center_y,
-    //                        const real_t line_x,
-    //                        const real_t line_y,
-    //                        const real_t line_angle,
-    //                        const ssize_t artifact_size,
-    //                        const real_t filter_x,
-    //                        const real_t filter_y,
-    //                        const real_t filter_radius,
-    //                        const size_t angle_samples)
-    // {
-    //     real_t fx, fy, lx, ly, sx, sy;
-
-    //     const auto R = ssize_t(filter_radius + 0.5);
-    //     const real_t maxR = filter_radius + artifact_size;
-    //     const auto c0 = ssize_t(filter_x - R);
-    //     const auto c1 = ssize_t(filter_x + R);
-    //     const auto r0 = ssize_t(filter_y - R);
-    //     const auto r1 = ssize_t(filter_y + R);
-    //     const auto minimum = std::numeric_limits<return_t>::min();
-    //     const auto maximum = std::numeric_limits<return_t>::max();
-
-    //     size_t sum = 0;
-    //     real_t summed_area = 0;
-
-    //     for (size_t a = 1; a <= angle_samples; ++a) {
-    //         const real_t offset = pi / (2 * a);
-    //         const real_t sincos_offset[] = {std::sin(offset), std::cos(offset)};
-    //         const real_t angle = line_angle - offset;
-    //         const real_t line_vector_x = std::cos(angle);
-    //         const real_t line_vector_y = std::sin(angle);
-
-    //         std::tie(fx, fy) =
-    //             rotate(sincos_offset, center_x, center_y, filter_x, filter_y);
-    //         std::tie(lx, ly) =
-    //             rotate(sincos_offset, center_x, center_y, line_x, line_y);
-
-    //         const real_t ld = lx * line_vector_x - ly * line_vector_y;
-    //         const real_t fd = fx * line_vector_x - fy * line_vector_y;
-    //         if (fd - maxR > ld)
-    //             return minimum;
-    //         if (fd + maxR < ld)
-    //             return maximum;
-
-    //         for (ssize_t sample_y = r0; sample_y <= r1; ++sample_y) {
-    //             for (ssize_t sample_x = c0; sample_x <= c1; ++sample_x) {
-    //                 std::tie(sx, sy) = rotate(
-    //                     sincos_offset, center_x, center_y, sample_x, sample_y);
-
-    //                 const auto area = filter_radius == 0
-    //                                       ? 1
-    //                                       : box_circle_area(sx - 0.5,
-    //                                                         sx + 0.5,
-    //                                                         sy - 0.5,
-    //                                                         sy + 0.5,
-    //                                                         fx,
-    //                                                         fy,
-    //                                                         filter_radius);
-
-    //                 const real_t raster_sx = rasterize(artifact_size, sx);
-    //                 const real_t raster_sy = rasterize(artifact_size, sy);
-
-    //                 const real_t d =
-    //                     raster_sx * line_vector_x - raster_sy * line_vector_y;
-
-    //                 sum += area * (d < ld ? maximum : minimum);
-    //                 summed_area += area;
-    //             }
-    //         }
-    //     }
-    //     return return_t(sum / summed_area + 0.5);
-    // }
 } // namespace
 
 template <typename pixel_t, typename real_t>
@@ -192,8 +118,6 @@ image_t<pixel_t> disk(const size_t width,
                       const size_t artifact_size,
                       const real_t filter_radius,
                       const pixel_t filter_noise,
-                    //   const pixel_t bg_noise,
-                    //   const size_t angle_samples,
                       const real_t image_angle)
 {
     typedef typename std::make_signed<pixel_t>::type spixel_t;
@@ -204,7 +128,6 @@ image_t<pixel_t> disk(const size_t width,
                                                                 filter_noise);
 
     image_t<pixel_t> result(height, width);
-    // const auto max_value = std::numeric_limits<pixel_t>::max();
 
     real_t filter_x, filter_y;
     const auto center_x = width * 0.5;
@@ -221,16 +144,6 @@ image_t<pixel_t> disk(const size_t width,
             std::tie(filter_x, filter_y) =
                 rotate(sincos, center_x, center_y, (real_t)c, (real_t)r);
 
-            // const pixel_t s = filter<pixel_t>(center_x,
-            //                                   center_y,
-            //                                   line_x,
-            //                                   line_y,
-            //                                   line_angle,
-            //                                   (ssize_t)artifact_size,
-            //                                   filter_x,
-            //                                   filter_y,
-            //                                   filter_radius,
-            //                                   angle_samples);
             const auto s = pixel_t(0.5 + minimum + maximum
                                          * filter_line(line_x,
                                                        line_y,
@@ -259,51 +172,4 @@ image_t<pixel_t> disk(const size_t width,
 
     return result;
 }
-
-// template <typename image_t, typename real_t>
-// image_t disk(const image_t& image,
-//              const real_t radius,
-//              const scalar_t<image_t> noise)
-// {
-//     typedef scalar_t<image_t> pixel_t;
-//     typedef typename std::make_signed<pixel_t>::type spixel_t;
-
-//     std::random_device rd;
-//     std::mt19937 generator(rd());
-//     std::uniform_int_distribution<spixel_t> random_noise(-noise, noise);
-
-//     image_t result(image.rows(), image.cols());
-//     const auto max_value = std::numeric_limits<pixel_t>::max();
-//     const auto R = size_t(radius + 0.5);
-//     const auto width = (size_t)image.cols();
-//     const auto height = (size_t)image.rows();
-
-//     for (size_t r = 0; r < height; ++r) {
-//         const size_t r0 = R > r ? 0 : r - R;
-//         const size_t r1 = std::min(r + R + 1, height);
-
-//         for (size_t c = 0; c < width; ++c) {
-//             const size_t c0 = R > c ? 0 : c - R;
-//             const size_t c1 = std::min(c + R + 1, width);
-
-//             const pixel_t s =
-//                 filter<size_t>(image, r0, r1, c0, c1, c, r, radius);
-//             result(r, c) = s;
-
-//             if (s != image(r, c)) {
-//                 const auto n = random_noise(generator);
-
-//                 if (n < 0) {
-//                     const auto n_ = -n;
-//                     result(r, c) -= n_ > s ? s : n_;
-//                 } else {
-//                     const auto s_ = max_value - s;
-//                     result(r, c) += n > s_ ? s_ : n;
-//                 }
-//             }
-//         }
-//     }
-
-//     return result;
-// }
 } // namespace psm
--- src/py/filter.cpp
+++ src/py/filter.cpp
@@ -16,8 +16,6 @@ PyObject* npy_disk(const size_t width,
                    const size_t artifact_size,
                    const double radius,
                    const int filter_noise,
-                //    const int bg_noise,
-                //    const size_t angle_samples,
                    const double image_angle)
 {
     auto result = psm::disk(width,
@@ -28,8 +26,6 @@ PyObject* npy_disk(const size_t width,
                             artifact_size,
                             radius,
                             (T)filter_noise,
-                            // (T)bg_noise,
-                            // angle_samples,
                             image_angle);
     return vps_py::py_object(result);
 }
@@ -43,8 +39,6 @@ extern "C" PyObject* disk(long width,
                           long artifact_size,
                           double radius,
                           long filter_noise,
-                        //   long bg_noise,
-                        //   long angle_samples,
                           double image_angle)
 {
     PyGIL lock;
@@ -58,38 +52,12 @@ extern "C" PyObject* disk(long width,
                                  (size_t)artifact_size,
                                  radius,
                                  (int)filter_noise,
-                                //  (int)bg_noise,
-                                //  (size_t)angle_samples,
                                  image_angle);
     } catch (...) {
         return exception2py();
     }
     return nullptr;
 }
-
-
-// template <typename T>
-// PyObject* npy_disk(PyObject* image, double radius, int noise)
-// {
-//     const Image<T> tmp = vps_py::Map<Image<T>>(image);
-//     auto result = psm::disk(tmp, radius, (T)noise);
-//     return vps_py::py_object(result);
-// }
-
-
-// extern "C" PyObject* disk(PyObject* image, double radius, long noise)
-// {
-//     PyGIL lock;
-
-//     try {
-//         EXCEPTION_IF(vps_py::get_type(image) != NPY_UINT8,
-//                      "Only uint8 images are supported.");
-//         return npy_disk<uint8_t>(image, radius, (int)noise);
-//     } catch (...) {
-//         return exception2py();
-//     }
-//     return nullptr;
-// }
 
 
 MOD_INIT(filter, 0)
