6bf1cfc3f430ea95f45ccf731c1ff62cba81c36a
Michael Hecher <mh@zbox.upc.at>
2019-07-23 23:01:08 +0200

    save

--- gl_demo.py
+++ gl_demo.py
@@ -2,10 +2,13 @@ import gi
 gi.require_version("Gtk", "3.0")
 gi.require_version("Gdk", "3.0")
 from gi.repository import Gtk, Gdk, GLib, GObject
+import time
 import cairo
 import numpy as np
 from OpenGL.GL import *
 import pyopencl as cl
+
+import psm.filter
 
 mem = cl.mem_flags
 
@@ -16,20 +19,27 @@ layout(location = 1) uniform vec2 scale;
 
 layout (location = 0) in vec4 pos;
 
+out vec2 uv;
+
 void main()
 {
     gl_Position.xy = pos.xy * scale.xy + shift.xy;
     gl_Position.zw = vec2(0, 1);
+    uv = pos.zw;
 }
 """
 
 fs = """
 #version 460 core
+layout(binding=0) uniform sampler2D tex;
+
+in vec2 uv;
+
 out vec4 color;
 
 void main()
 {
-    color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    color = texture(tex, uv);
 }
 """
 
@@ -84,14 +94,20 @@ class Gui:
         self.builder.add_from_file(glade_file)
         self.builder.connect_signals(handlers)
 
-        self._texture = None
+        self._textures = None
+        self._reference_image = None
+        self._artifact_image = None
         self._vertex_buffer = None
         self._vertex_object = None
         self._vertex_shader = None
         self._fragment_shader = None
         self._shader_program = None
-        self._context = None
-        self._command_queue = None
+
+        self._draw_line = None
+        self._draw_artifact_line = None
+
+        self._start_time = time.time()
+        self._last_time = self._start_time
 
         self.window = self.builder.get_object("window")
         self.window.show_all()
@@ -100,30 +116,42 @@ class Gui:
         ctx = gl_area.get_window().create_gl_context()
         ctx.set_required_version(4, 6)
         ctx.set_debug_enabled(True)
-        print("ctx realize:", ctx.realize())
+        ctx.realize()
         ctx.make_current()
 
-        from pyopencl.tools import get_gl_sharing_context_properties
-        platform = cl.get_platforms()[-1]
-        devices = platform.get_devices()
-        properties = [(cl.context_properties.PLATFORM, platform)
-                      ] + get_gl_sharing_context_properties()
-        self._context = cl.Context(devices=devices, properties=properties)
-        self._command_queue = cl.CommandQueue(self._context)
+        psm.filter.init_opencl()
 
         return ctx
 
     def on_realize(self, gl_area):
         gl_area.get_context().make_current()
 
-        self._texture = glGenTextures(1)
-        glBindTexture(GL_TEXTURE_2D, self._texture)
+        self._textures = glGenTextures(2)
+        self._reference_image = self._textures[0]
+        self._artifact_image = self._textures[1]
+
+        glActiveTexture(GL_TEXTURE0)
+        glBindTexture(GL_TEXTURE_2D, self._reference_image)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 800, 800, 0, GL_RGB,
                      GL_UNSIGNED_BYTE, None)
-        t = cl.GLTexture(self._context, mem.READ_ONLY, GL_TEXTURE_2D, 0, self._texture, dims=2)
+        self._draw_line = psm.filter.Line(800, 800, self._reference_image)
+
+        glBindTexture(GL_TEXTURE_2D, self._artifact_image)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 800, 800, 0, GL_RGB,
+                     GL_UNSIGNED_BYTE, None)
+        self._draw_artifact_line = psm.filter.ArtifactLine(
+            800, 800, self._artifact_image)
 
         vertex_data = np.array(
-            [[-1, -1, 0, 1], [1, -1, 0, 1], [1, 1, 0, 1], [-1, 1, 0, 1]],
+            [[-1, -1, 0, 0], [1, -1, 1, 0], [1, 1, 1, 1], [-1, 1, 0, 1]],
             np.float32)
 
         self._vertex_buffer = glGenBuffers(1)
@@ -138,7 +166,6 @@ class Gui:
         vertex_shader = createShader(GL_VERTEX_SHADER, vs)
         fragment_shader = createShader(GL_FRAGMENT_SHADER, fs)
         self._program = createProgram(vertex_shader, fragment_shader)
-
         glDeleteShader(vertex_shader)
         glDeleteShader(fragment_shader)
 
@@ -147,27 +174,43 @@ class Gui:
     def on_unrealize(self, gl_area):
         gl_area.get_context().make_current()
 
-        glDeleteTextures([self._texture])
+        self._draw_line = None
+        self._draw_artifact_line = None
+
+        glDeleteTextures(self._textures)
         glDeleteBuffers(1, [self._vertex_buffer])
         glDeleteVertexArrays(1, [self._vertex_object])
         glDeleteProgram(self._program)
-
-        self._command_queue = None
-        self._context = None
 
     def on_resize(self, gl_area, width, height):
         pass
 
     def on_render(self, gl_area, gl_context):
+        current_time = time.time()
+        animation_time = self._start_time - current_time
+        elapsed_time = self._last_time - current_time
+        self._last_time = current_time
+        self.window.set_title(f'User Study {int(1.0/elapsed_time)} fps')
+
+        self._draw_line(400.0, 400.0, animation_time * np.pi / 4, 100.0, 10.0 / 255.0, 0.0)
+        self._draw_artifact_line(400.0, 400.0, animation_time * np.pi / 4, 8, 100.0, 10.0 / 255.0, 0.0)
+
         glClear(GL_COLOR_BUFFER_BIT)
+
         glUseProgram(self._program)
         glBindVertexArray(self._vertex_object)
+
         glUniform2f(0, -0.5, 0.0)
         glUniform2f(1, 0.5, 1.0)
+        glBindTexture(GL_TEXTURE_2D, self._reference_image)
         glDrawArrays(GL_TRIANGLE_FAN, 0, 4)
+
         glUniform2f(0, 0.5, 0.0)
         glUniform2f(1, 0.5, 1.0)
+        glBindTexture(GL_TEXTURE_2D, self._artifact_image)
         glDrawArrays(GL_TRIANGLE_FAN, 0, 4)
+
+        gl_area.queue_draw()
         return True
 
 
--- psm/filter.py
+++ psm/filter.py
@@ -4,26 +4,26 @@ import numpy as np
 import pyopencl as cl
 from pyopencl.tools import get_gl_sharing_context_properties
 
-
 context = None
 command_queue = None
 mem = cl.mem_flags
 
+
 def init_opencl():
     global context, command_queue
+    if context is not None:
+        return
     platform = cl.get_platforms()[-1]
     devices = platform.get_devices()
-    context = cl.Context(devices=devices,
-                        properties=[(cl.context_properties.PLATFORM, platform)] +
-                        get_gl_sharing_context_properties())
+    context = cl.Context(
+        devices=devices,
+        properties=[(cl.context_properties.PLATFORM, platform)] +
+        get_gl_sharing_context_properties())
     command_queue = cl.CommandQueue(context)
 
 
 class Base:
-    context = None
-    command_queue = None
-
-    def __init__(self, image_width, image_height, opencl_file):
+    def __init__(self, image_width, image_height, opencl_file, gl_image=None):
         init_opencl()
 
         with open(opencl_file, 'r') as f:
@@ -33,16 +33,26 @@ class Base:
             context,
             source).build(options=['-I', f'"{os.path.dirname(opencl_file)}"'])
 
-        shape = (image_height, image_width)
-        fmt = cl.ImageFormat(cl.channel_order.RGBA, cl.channel_type.UNORM_INT8)
-        self._result_image = cl.Image(context, mem.WRITE_ONLY, fmt, shape)
+        if gl_image is None:
+            shape = (image_height, image_width)
+            fmt = cl.ImageFormat(cl.channel_order.RGBA, cl.channel_type.UNORM_INT8)
+            self._result_image = cl.Image(context, mem.WRITE_ONLY, fmt, shape)
+        else:
+            from OpenGL.GL import GL_TEXTURE_2D
+            self._result_image = cl.GLTexture(context,
+                                              mem.WRITE_ONLY,
+                                              GL_TEXTURE_2D,
+                                              0,
+                                              gl_image,
+                                              dims=2)
 
 
 class Line(Base):
-    def __init__(self, image_width, image_height):
+    def __init__(self, image_width, image_height, gl_image=None):
         super().__init__(
             image_width, image_height,
-            os.path.join(os.path.dirname(__file__), 'filtered_line.cl'))
+            os.path.join(os.path.dirname(__file__), 'filtered_line.cl'),
+            gl_image=gl_image)
 
     def __call__(self,
                  line_x: float,
@@ -54,6 +64,8 @@ class Line(Base):
                  result: np.array = None) -> np.array:
         shape = self._result_image.shape
 
+        if hasattr(self._result_image, 'gl_object'):
+            cl.enqueue_acquire_gl_objects(command_queue, [self._result_image])
         self._program.filtered_line(command_queue, shape, None,
                                     np.uint32(shape[1]), np.uint32(shape[0]),
                                     np.float32(line_x), np.float32(line_y),
@@ -62,23 +74,25 @@ class Line(Base):
                                     np.float32(filter_noise),
                                     np.float32(image_angle),
                                     self._result_image)
+        if hasattr(self._result_image, 'gl_object'):
+            cl.enqueue_release_gl_objects(command_queue, [self._result_image])
 
-        if result is None:
-            result = np.zeros(shape + (4, ), np.uint8)
-        cl.enqueue_copy(command_queue,
-                        result,
-                        self._result_image,
-                        origin=(0, 0),
-                        region=shape)
-        return result
+        if result is not None:
+            cl.enqueue_copy(command_queue,
+                            result,
+                            self._result_image,
+                            origin=(0, 0),
+                            region=result.shape)
+            return result
 
 
 class ArtifactLine(Base):
-    def __init__(self, image_width, image_height):
+    def __init__(self, image_width, image_height, gl_image=None):
         super().__init__(
             image_width, image_height,
             os.path.join(os.path.dirname(__file__),
-                         'filtered_line_artifact.cl'))
+                         'filtered_line_artifact.cl'),
+            gl_image=gl_image)
 
     def __call__(self,
                  line_x: float,
@@ -91,6 +105,8 @@ class ArtifactLine(Base):
                  result: np.array = None) -> np.array:
         shape = self._result_image.shape
 
+        if hasattr(self._result_image, 'gl_object'):
+            cl.enqueue_acquire_gl_objects(command_queue, [self._result_image])
         self._program.filtered_line_artifact(command_queue, shape, None,
                                              np.uint32(shape[1]),
                                              np.uint32(shape[0]),
@@ -102,12 +118,13 @@ class ArtifactLine(Base):
                                              np.float32(filter_noise),
                                              np.float32(image_angle),
                                              self._result_image)
+        if hasattr(self._result_image, 'gl_object'):
+            cl.enqueue_release_gl_objects(command_queue, [self._result_image])
 
-        if result is None:
-            result = np.zeros(shape + (4, ), np.uint8)
-        cl.enqueue_copy(command_queue,
-                        result,
-                        self._result_image,
-                        origin=(0, 0),
-                        region=shape)
-        return result
+        if result is not None:
+            cl.enqueue_copy(command_queue,
+                            result,
+                            self._result_image,
+                            origin=(0, 0),
+                            region=result.shape)
+            return result
