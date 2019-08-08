07744d30741b0dbdbab755efa78af41833c631e3
Michael Hecher <mh@zbox.upc.at>
2019-08-08 23:10:16 +0200

    Save

--- evaluation.py
+++ evaluation.py
@@ -7,6 +7,15 @@ import os
 import sys
 
 
+def merge_structured_arrays(*arrays):
+    array_out = np.empty((sum(len(a) for a in arrays),), dtype=arrays[0].dtype)
+    i = 0
+    for a in arrays:
+        array_out[i:(i+a.shape[0])] = a
+        i += a.shape[0]
+    return array_out
+
+
 parser = argparse.ArgumentParser(description='Evaluate study data.')
 
 parser.add_argument('--user', help='Evaluate a single user.')
@@ -16,13 +25,13 @@ parser.add_argument('--thresholds', help='Show the threshold plot.', action="sto
 if len(sys.argv) > 1:
     args = parser.parse_args(sys.argv[1:])
 else:
-    args = parser.parse_args(['--threshold', '--stairs'])
+    args = parser.parse_args(['--thresholds'])
 
 user_filter = '*' if args.user is None else args.user
 files = glob.glob(os.path.join('data', f'{user_filter}.csv'))
 
 tables = [np.genfromtxt(f, delimiter=',', names=True, dtype=None, encoding='utf-8') for f in files]
-table = np.concatenate(tables)
+table = merge_structured_arrays(*tables)
 
 if args.stairs:
     data = table[table['is_reference'] == False]
@@ -61,7 +70,6 @@ if args.thresholds:
             r = radius[(user == user_name) & (label == label_name)]
             label_color = (label_index + 1) / len(unique_label)
             color = matplotlib.colors.hsv_to_rgb((label_color, user_color, 0.8))
-            ax.plot(r[-1], 'o', color=color, label=label_name)
-    ax.legend()
+            ax.plot(label_index, r[-1], 'o', color=color)
 
 plt.show()
